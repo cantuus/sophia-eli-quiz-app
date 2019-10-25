69d7050d7d1252d7d5456b08aa5d35f25237765e
Sophia Koeut <koeutsophia@gmail.com>
2019-10-25 14:22:05 -0700

    updated main.js

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+function clickStartQuiz() {
+$('#start-button').click(function(){
+    $()
+})
+};
+
+function updateScore() {
+    
+};
+
+function updateQuestionNumber() {
+
+}
+
+function displayQuestion(){
+
+};
+
+function displayQuestionChoices(){
+
+}
+
+function displayResults(){
+
+}
+
+function handleSelection(){
+
+}
+
+function restartQuiz (){
+
+}
--- scripts/store.js
+++ scripts/store.js
@@ -1,14 +1,78 @@
-// A pickaxe is a basic, commonly-used tool in which videogame?
-// Mincraft
+const store ={
+    questions: [//1
+    {
+        question: "A pickaxe is a basic, commonly-used tool in which videogame?",   
+        options: [
+            "Call of Duty",
+            "Minecraft",
+            "Metroid",
+            "Pokemon",
+            "Golden-Eye",
+        ],
+            answer:"Minecraft"
+    },
+    {
+        questions:
+        {
+            question: "What are the professions of Mario and his brother Luigi?",   
+            options: [
+            "Accountants",
+            "Carpenters",
+            "Developers",
+            "Therapists",
+            "Plumbers",
+        ],
+                answer:"Plumbers"
+        },
+        //3
+        {
+            question: "What is the name of the racing series that is a spinoff of 'Super Mario Brothers'"?,   
+            options: [
+                "Speed Racer",
+                "Mario Kart",
+                "Forza Horizon",
+                "Project Gotham",
+                "Some racing game I can't think of",
+            ],
+                answer:"Mario Kart"
+        },
+        //4
+        {
+            question: "The word "Pokémon" is generally understood to be a translation of which phrase?",   
+            options: [
+                "Pop Monsters",
+                "Poster Monstrel",
+                "Poke the Monster",
+                "Pom Pom Mon",
+                "Pocket Monsters",
+            ],
+                answer:"Pocket Monsters"
+        },
+        //5
+        {
+            question: "What color do the ghost enemies turn to once Pac-Man eats a Power Pellet?",   
+            options: [
+                "Blue",
+                "Gray",
+                "White",
+                "Red",
+                "Purple",
+            ],
+                answer:"Blue"
+        },
+    ],
+    currentQuestion = 0,
+    score = 0
 
-// What are the professions of Mario and his brother Luigi?
-// Plumbers
-
-// What is the name of the racing series that is a spinoff of "Super Mario Brothers"?
-// Mario Kart
-
-// The word "Pokémon" is generally understood to be a translation of which phrase?
-// Pocket Monsters
-
-// What color do the ghost enemies turn to once Pac-Man eats a Power Pellet?
-// Blue
+};
+    
+        
+        
+        
+        
+        
+        
+    
+        
+        
+        
