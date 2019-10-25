30778d41e03e0101826d94ca98c9e7de79b7c463
Sophia Koeut <koeutsophia@gmail.com>
2019-10-25 12:14:14 -0700

    first commit

---
+++ scripts/store.js
@@ -0,0 +1,14 @@
+// A pickaxe is a basic, commonly-used tool in which videogame?
+// Mincraft
+
+// What are the professions of Mario and his brother Luigi?
+// Plumbers
+
+// What is the name of the racing series that is a spinoff of "Super Mario Brothers"?
+// Mario Kart
+
+// The word "Pokémon" is generally understood to be a translation of which phrase?
+// Pocket Monsters
+
+// What color do the ghost enemies turn to once Pac-Man eats a Power Pellet?
+// Blue
